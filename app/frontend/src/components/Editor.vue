d7517cddac2980700ed48a773ff72c997badf7a4
Clark Winters <dev@clarkwinters.com>
2024-01-18 07:47:45 -0600

    switch back to TinyMCE

--- app/frontend/src/components/Editor.vue
+++ app/frontend/src/components/Editor.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { useEditor, EditorContent } from "@tiptap/vue-3"
-import StarterKit from "@tiptap/starter-kit"
-
-const editor = useEditor({
-  content: "<h1>Hello, Webster!</h1>",
-  extensions: [StarterKit],
-})
-
-console.log({ html: editor.value.getHTML() })
+import Editor from "@tinymce/tinymce-vue"
 </script>
 
 <template>
-  <editor-content :editor="editor" />
+  <div class="container">
+    <Editor :init="{ height: '100vh' }"><h1>Hello, Webster!</h1></Editor>
+  </div>
 </template>
+
+<style scoped>
+/* .container {
+  border: 5px black;
+  border-radius: 10px;
+} */
+</style>
